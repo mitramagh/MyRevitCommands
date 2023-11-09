65d37476a612ee835c93ffd1ef825d58ac858d42
mitramagh <mit.magh@gmail.com>
2023-11-09 12:22:57 -0800

    Added GetTypeID

--- MyRevitCommands/GetElementId.cs
+++ MyRevitCommands/GetElementId.cs
@@ -13,16 +13,33 @@ namespace MyRevitCommands
             //Get UIDocument
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
 
+            // Get Document
+            Document doc = uidoc.Document;
+
             try
             {
 
                 //Pick object
                 Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
 
+                //Retrieve Element
+                ElementId eleId = pickedObj.ElementId;
+                //if (eleId == null) { }
+
+                Element ele = doc.GetElement(eleId);
+
+                //Get Element Type
+                ElementId eTypeId = ele.GetTypeId();
+                ElementType eType = doc.GetElement(eTypeId) as ElementType;
+
                 //Display Element ID
                 if (pickedObj != null)
                 {
-                    TaskDialog.Show("Element Id", pickedObj.ElementId.ToString());
+                    TaskDialog.Show("Element Classification", eleId.ToString() + Environment.NewLine
+                        + "Category: " + ele.Category.Name + Environment.NewLine
+                        + "Instance: " + ele.Name + Environment.NewLine
+                        + "Symbol: " + eType.Name + Environment.NewLine
+                        + "Family: " + eType.FamilyName); 
                 }
 
                 return Result.Succeeded;
